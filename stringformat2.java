623b56a6fd5513d14cb8f85e3e427e42792bd621
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-02 19:50:33 +0530

    Add files via upload

---
+++ stringformat2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class stringformat2{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=sc.nextInt();
+        System.out.println(String.format("THE NAME OF THE PERSON IS %s and AGE IS %d",a,b));
+    }
+
+    }
