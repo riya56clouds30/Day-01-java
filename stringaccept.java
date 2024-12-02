80a352d259036df7eb20eab417f53d099bc36096
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-02 16:23:06 +0530

    Add files via upload

---
+++ stringaccept.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class stringaccept{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("CONGRATUALATION "+a+" you won 5 lakhs");
+    
+    }
+}
